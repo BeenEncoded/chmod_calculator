917a038594874ff16ea30c1fc09c26bde3dbfbe3
Jonathan Whitlock <beenencoded@outlook.com>
2022-09-04 19:47:10 -0400

    Adding vscode stuffs

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,4 @@
 int main(int c __attribute__((unused)), char** v __attribute__((unused)))
 {
     std::cout<< "Hello World!"<< std::endl;
-    if(c > 0)
-    {
-        std::cout<< "C = "<< c<< std::endl;
-    }
 }

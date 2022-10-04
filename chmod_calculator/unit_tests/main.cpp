d1e7f64fc3fa31c9676c5e76b0fbd7f854286f33
Jonathan Whitlock <beenencoded@outlook.com>
2022-10-03 22:57:46 -0400

    added some doxygentation

--- chmod_calculator/unit_tests/main.cpp
+++ chmod_calculator/unit_tests/main.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file main.cpp
+ * @author Jonathan Whitlock (beenencoded@outlook.com)
+ * @brief Main entry point for unit testing.
+ * @version 0.1
+ * @date 2022-10-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <iostream>
 #include <boost/version.hpp>
 

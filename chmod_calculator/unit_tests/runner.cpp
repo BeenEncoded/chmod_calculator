699208b7f9a5b0ce510e958c9bf9456c906f073a
Jonathan Whitlock <beenencoded@outlook.com>
2022-10-16 15:24:51 -0400

    added initialization to the test program -- namely seeding random number generation

--- chmod_calculator/unit_tests/runner.cpp
+++ chmod_calculator/unit_tests/runner.cpp
@@ -10,4 +10,35 @@
  */
  #define BOOST_TEST_MODULE "Tests for chmod_calculator"
  #include <boost/test/unit_test.hpp>
+ #include <stdio.h>
+ #include <time.h>
 
+namespace
+{
+    /**
+     * @brief Initializes and uninitializes the test program.
+     * 
+     */
+    struct GlobalInitializationFixture
+    {
+        GlobalInitializationFixture()
+        {
+            // seed our pseudo-random number generation
+            srand(time(nullptr));
+        }
+
+        ~GlobalInitializationFixture()
+        {
+        }
+
+        void setup()
+        {
+        }
+
+        void teardown()
+        {
+        }
+    };
+}
+
+BOOST_TEST_GLOBAL_FIXTURE(GlobalInitializationFixture);
